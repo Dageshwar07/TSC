23a02237641d4537b602d09be5861b7ce77c83b6
Dageshwar07 <dageshwardasmanikpuri996@gmail.com>
2024-10-22 10:12:13 +0530

    array

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ var a = 5;
 function abcd() {
     console.log(a);
 }
+var c = [1, 2, 3, 4, 5];
+var cCopy = [...c]; 
+var d = cCopy.pop(); 
+console.log(c);     
+console.log(cCopy);  
--- app.ts
+++ app.ts
@@ -2,3 +2,7 @@ var a =5;
 function abcd(){
     console.log(a)
 }
+
+var c = [1,2,3,4,5]
+var d = c
+console.log(d)
